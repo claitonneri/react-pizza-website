cc7349d9bfce921df77cec9ab4d40bc380382619
Claiton Neri <claitondidone@guararapes.com.br>
2020-12-16 15:36:54 -0300

    ⚙️ feat: criação inicial do componente NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import NavBar from './components/NavBar';
+
 const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default App;
